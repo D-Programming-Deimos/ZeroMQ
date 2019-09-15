3ee58f16cfe509deb13421ea2b2cb9b55398d29e
Lars T. Kyllingstad <post-dev@larskyllingstad.no>
2019-09-15 21:55:11 +0200

    Update to ZeroMQ 4.2.5

--- deimos/zmq/zmq.d
+++ deimos/zmq/zmq.d
@@ -44,7 +44,7 @@ nothrow extern (C)
 /*  Version macros for compile-time API version detection                     */
 enum ZMQ_VERSION_MAJOR = 4;
 enum ZMQ_VERSION_MINOR = 2;
-enum ZMQ_VERSION_PATCH = 4;
+enum ZMQ_VERSION_PATCH = 5;
 
 int ZMQ_MAKE_VERSION(int major, int minor, int patch)
 {
